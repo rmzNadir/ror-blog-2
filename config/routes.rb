d9e2e72c8b38adb2af91c6612ed9ee258ebca50f
rmzNadir <dgzrz99@gmail.com>
2021-02-17 17:27:56 -0600

    :construction: root path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'bienvenida', to: 'home#index'
 
+  root to: "home#index"
+
   get 'articles/:id', to: 'articles#show'
   get 'articles/:id/edit', to: 'articles#edit'
   get 'articles/new', to: 'articles#new'
