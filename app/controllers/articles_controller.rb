2f0d5b73e5006d589932db5a44937db5f0a9c87f
rmzNadir <dgzrz99@gmail.com>
2021-03-04 13:54:17 -0600

    :sparkles: protected routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class ArticlesController < ApplicationController
   before_action :find_article, except: [:new, :create, :index]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destory]
 
   def index
     @articles = Article.all
