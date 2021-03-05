2ca9a0d8a7e80b894c92fa5283d3083cec45d759
rmzNadir <dgzrz99@gmail.com>
2021-03-05 14:11:25 -0600

    :sparkles: strong params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article.update(title: params[:article][:title], content: params[:article][:content])
+    @article.update(article_params)
     redirect_to(@article)
   end
 
@@ -23,7 +23,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = current_user.articles.create(title: params[:article][:title], content: params[:article][:content])
+    @article = current_user.articles.create(article_params)
 
     redirect_to(@article)
   end
@@ -39,5 +39,9 @@ class ArticlesController < ApplicationController
 
   def find_article
     @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
