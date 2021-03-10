d930a1fb76eb8467bd0127a2d1bb1e0156a7a360
rmzNadir <dgzrz99@gmail.com>
2021-03-10 16:05:22 -0600

    :bug: fixed article category deletion

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,6 +8,10 @@ class Article < ApplicationRecord
 
   def save_categories
     # category_elements: [1,2,3]
+    return has_categories.destroy_all if category_elements.nil? || category_elements.empty?
+
+    has_categories.where.not(category_id: category_elements).destroy_all
+
     category_elements.each do |category_id|
       HasCategory.find_or_create_by(article: self, category_id: category_id)
     end
